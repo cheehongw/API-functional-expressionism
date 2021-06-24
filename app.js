c860399e5c24143e89cc73dd9f72861209bcbb6f
Chee Hong <c.h.wong2606@gmail.com>
2021-06-25 01:01:21 +0800

    Secured environment variables.

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var locationRouter = require('./routes/location');
 
 var app = express();
 
 //Setup mongoose connection
-let mongoDB = 'mongodb+srv://CheeHong-dev:ZNpXmAq8EkazyShY@tinfood-api.my9fd.mongodb.net/functional_expressionism_API?retryWrites=true&w=majority';
+let mongoDB = `${DB_CONNECTION}`;
 mongoose.connect(mongoDB, { useNewUrlParser:true, useUnifiedTopology: true });
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
