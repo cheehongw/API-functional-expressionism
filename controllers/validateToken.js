c0d204a7ce9f96dffadeb7d929de3ae856865de1
Chee Hong <c.h.wong2606@gmail.com>
2021-07-19 22:58:00 +0800

    Add firebase authentication middleware

---
+++ controllers/validateToken.js
@@ -0,0 +1,26 @@
+const admin = require('firebase-admin');
+const serviceAccount = require(`${process.env.GOOGLE_APPLICATION_CREDENTIALS}`);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+function validateToken(req, res, next) {
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    admin
+        .auth()
+        .verifyIdToken(token)
+        .then((decodedToken) => {
+            const uid = decodedToken.uid;
+            res.locals.uid = uid;
+            next();
+        })
+        .catch((err) => {
+            res.status(401).send(err);
+        })
+}
+
+module.exports = validateToken;
